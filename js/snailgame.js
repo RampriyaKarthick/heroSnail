c033f31349fd2787dd7a228d5e134eb802afb81d
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-03-18 10:13:58 +0530

    level, sound, live done

--- js/snailgame.js
+++ js/snailgame.js
@@ -73,7 +73,7 @@ heroImg.onload = function() {
   let speed = 2;
   let score=0;
   let greenyFrequency = 0.02;
-  let enemyFrequeny=0.07; 
+  //let enemyFrequeny=0.07; 
   let lives=3;   
 
  const obstacleImg = new Image();
@@ -86,6 +86,13 @@ heroImg.onload = function() {
 
   const livesImg = new Image();
   livesImg.src = "images/lives.png";
+
+  let level = 1;
+let scoreThresholds = [0, 50, 100, 200, 400]; // Score thresholds for each level
+let currentScoreThreshold = scoreThresholds[level];
+
+const gameOverSound = new Audio("music/planktonCollision.wav");
+
 
 
 function createObstacle() {
@@ -149,6 +156,7 @@ function updateObstacles() {
 }
 let collisionCount = 0;
 let gameOver = false; // Add a game over flag
+let gameOverSoundPlayed = false;
 
 function updateEnemies() {
   for (let i = 0; i < enemies.length; i++) {
@@ -170,7 +178,12 @@ function updateEnemies() {
         score -= 10;
         collisionCount++;
         if (collisionCount >= 3 && !gameOver) { // Check if game is over and game over message is not displayed
-          gameOver = true; // Set the game over flag to true
+          if (collisionCount >= 3 && !gameOver && !gameOverSoundPlayed) 
+            gameOverSoundPlayed = true; // Set gameOverSoundPlayed to true so the game over sound won't be played again
+            gameOver = true;
+            // play game over sound
+            gameOverSound.play(); // Set the game over flag to true
+        
           const gameOverCanvas = document.createElement("canvas");
           gameOverCanvas.width = window.innerWidth;
           gameOverCanvas.height = window.innerHeight;
@@ -207,80 +220,87 @@ function updateEnemies() {
     ctx.drawImage(heroImg, heroX, heroY, 175, 175);
   }
 
-  function drawLives() {
-      context.drawImage(livesImg, canvas.width - 130, 20, 25, 25);
-      context.fillText("x " + lives, canvas.width - 95, 40);
-    }
+ 
+    function updateGame() {
+
       
-    function draw() {
-      if (isGameOver) {
+        // ...
       
-          // Display game over screen and restart button
-          const gameOverScreen = document.getElementById('game-over');
-          const finalScore = document.getElementById('final-score');
-          finalScore.textContent = score;
-          gameOverScreen.style.display = 'block';
-      
-  }
+    
+      // Clear canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      // Draw background
+      context.drawImage(image, -canvasX, 0, canvasWidth, canvas.height);
+      context.drawImage(image, canvasWidth - canvasX, 0, canvasWidth, canvas.height);
+  
+      // Draw hero
       drawHero();
+  
       drawObstacles();
-      updateObstacles();
+  
       drawEnemies();
+  
+      updateObstacles();
+  
       updateEnemies();
-      drawScore();
-      drawLives();
-      
-    }
-  function updateGame() {
-    
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw background
-    context.drawImage(image, -canvasX, 0, canvasWidth, canvas.height);
-    context.drawImage(image, canvasWidth - canvasX, 0, canvasWidth, canvas.height);
-
-    // Draw hero
-    drawHero();
-
-    drawObstacles();
-
-    drawEnemies();
-
-    updateObstacles();
-
-    updateEnemies();
-
-
-//Draw Score
-context.font = "bold 24px Arial";
-    context.fillStyle = "#ffffff";
-    context.textAlign = "right";
-    context.fillText(`Score: ${score}`, canvas.width - 20, canvas.height - 20);
-
-    
-
-    // Move hero
-    if (isGoingUp) {
-      heroY -= 5;
-    } else if (isGoingDown) {
-      heroY += 5;
-    }
-
-    // Keep hero within canvas
-    if (heroY < 0) {
-      heroY = 0;
-    } else if (heroY > canvas.height - 100) {
-      heroY = canvas.height - 100;
-    }
-
-    // Move canvas
-    if (!isGameOver && !isPaused) {
-      canvasX += speed;
-      if (canvasX >= canvasWidth) {
-        canvasX = 0;
+  
+      //Draw Score
+      context.font = "bold 24px Arial";
+      context.fillStyle = "#ffffff";
+      context.textAlign = "right";
+      context.fillText(`Score: ${score}`, canvas.width - 20, canvas.height - 20);
+  
+      // Draw Level
+      context.font = "bold 24px Arial";
+      context.fillStyle = "#ffffff";
+      context.textAlign = "left";
+      context.fillText(`Level: ${level}`, 20, canvas.height - 20);
+  
+      // Check for level up
+      if (score >= currentScoreThreshold) {
+          level++;
+          currentScoreThreshold = scoreThresholds[level];
+          speed += 1; // Increase speed of obstacles and enemies
       }
+
+      
+    // Draw Lives
+    context.font = "bold 24px Arial";
+    context.fillStyle = "#ffffff";
+    context.textAlign = "center";
+    context.fillText(`Lives: ${lives}`, 20, 30);
+
+    // Increase lives for every 50 points, up to a maximum of 3
+    if (score % 50 === 0 && score > 0 && lives < 3) {
+      lives++;
+      // Play sound for gaining a life
+      gainLifeSound.play();
     }
+  
+      // Move hero
+      if (isGoingUp) {
+          heroY -= 5;
+      } else if (isGoingDown) {
+          heroY += 5;
+      }
+  
+      // Keep hero within canvas
+      if (heroY < 0) {
+          heroY = 0;
+      } else if (heroY > canvas.height - 100) {
+          heroY = canvas.height - 100;
+      }
+  
+      // Move canvas
+      if (!isGameOver && !isPaused) {
+        
+          canvasX += speed;
+          if (canvasX >= canvasWidth) {
+              canvasX = 0;
+          }
+      }
+  
 
     // Request next frame
     if (!isGameOver && !isPaused) {
